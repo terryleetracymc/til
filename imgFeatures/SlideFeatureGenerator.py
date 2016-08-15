65dafdbd3248eebae86cdbe1171735a62556ce24
风进 <liteng.lt@alibaba-inc.com>
2016-08-15 23:57:13 +0800

    双+1-1线搞定

--- imgFeatures/SlideFeatureGenerator.py
+++ imgFeatures/SlideFeatureGenerator.py
@@ -18,14 +18,14 @@ def generate_slide_feature(split_size=2,
                            ex=100):
     feature_objs = []
     # 画中心点两条直线
-    mid_y = int(feature_width / 2)
+    mid_y = int(feature_height / 2)
     s_mid_y = mid_y - 1
-    mid_x = int(feature_height / 2)
+    mid_x = int(feature_width / 2)
     s_mid_x = mid_x - 1
     if s_mid_y >= 0:
         # 图都不大搞毛
         for i in range(0, split_size + 1):
-            blank_image = np.zeros((feature_width, feature_height, 1), np.float64)
+            blank_image = np.zeros((feature_height, feature_width, 1), np.float64)
             blank_image[:, :] = midv
             delta = rad_range / split_size * i
             if delta == 0:
@@ -47,16 +47,29 @@ def generate_slide_feature(split_size=2,
             else:
                 tan_delta = math.tan(delta)
                 plus_start_point = (0, int(mid_y - mid_x * tan_delta))
-                plus_end_point = (feature_width, int(tan_delta * (feature_width - mid_x) + mid_y))
+                plus_end_point = (feature_height, int(tan_delta * (feature_height - mid_x) + mid_y))
                 cv2.line(blank_image, plus_start_point, plus_end_point, hv)
                 # 赋值凹点
-                minus_image = np.zeros((feature_width, feature_height, 1), np.float64)
-                for n in range(0, feature_height):
-                    for m in range(0, feature_width - 1):
-                        value = blank_image[m, n]
-                        if value == hv:
-                            minus_image[m + 1, n] = lv
-                blank_image = blank_image + minus_image
+                for m in range(0, feature_width):
+                    h_time = 0
+                    h_start = -1
+                    for n in range(0, feature_height):
+                        value = blank_image[n, m]
+                        if h_start == -1 and value == hv:
+                            h_start = n
+                            h_time += 1
+                        elif value == hv and h_start != -1:
+                            h_time += 1
+                        elif h_time != 0 and value == midv:
+                            break
+                    #
+                    if h_start != -1:
+                        for n in range(h_start + h_time, h_start + 2 * h_time + 1):
+                            if n < feature_height and h_time > 0:
+                                blank_image[n, m] = -1
+                                h_time -= 1
+                            else:
+                                break
             feature_obj = {"feature": blank_image, "angle": delta}
             feature_objs.append(feature_obj)
     return feature_objs
--- main.py
+++ main.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     # dst_width = 500
     # dst_height = 500
     # 生成各种特征
-    features_obj_array = generate_slide_feature(500, 50, 50, rad_range=math.pi / 2)
+    features_obj_array = generate_slide_feature(10, 5, 5, rad_range=math.pi / 2)
     # 读取图像
-    src_img = cv2.imread("/home/terry/Desktop/cat.jpg", cv2.CV_LOAD_IMAGE_GRAYSCALE)
+    src_img = cv2.imread("/home/terry/FERET/FERET-001/01.tif", cv2.CV_LOAD_IMAGE_GRAYSCALE)
     # src_img = cv2.resize(src_img, (dst_width, dst_height))
     # 与特征做卷积
     cv2.imshow("showme", src_img)
@@ -22,10 +22,11 @@ if __name__ == "__main__":
     for feature_obj in features_obj_array:
         dst = cv2.filter2D(src_img, -1, feature_obj['feature'])
         angle = feature_obj['angle'] * 180 / math.pi
-        cv2.imwrite("/home/terry/features/" + str(angle) + ".jpg", dst)
+        print angle
+        # cv2.imwrite("/home/terry/features/" + str(angle) + ".jpg", dst)
         cv2.imshow("showme", dst)
         cv2.imshow("feature", feature_obj['feature'])
-        cv2.waitKey(1)
-        time.sleep(0.01)
+        cv2.waitKey(0)
+        # time.sleep(1)
     cv2.waitKey(0)
 
